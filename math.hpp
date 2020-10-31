31e99f6b116b0feffd896ebd4139230ee9c89827
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2020-10-31 11:16:04 +0300

    [master] General-purpouse constexpr math functions

---
+++ math.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <stdexcept>
+
+/*
+ * general-purpous math function
+ */
+namespace mlp
+{
+template<typename T>
+constexpr auto pow(T x, int n) -> T
+{
+  if (n == 0)
+    return T(1);
+  if (n < 0)
+    return T(1) / pow(x, -n);
+
+  auto x_n = 1.0;
+  for (; n > 0; n /= 2)
+  {
+    if (n & 1)
+      x_n *= x;
+    x *= x;
+  }
+  return x_n;
+}
+
+constexpr auto exp(double x) -> double
+{
+  if (x < -3.0 || 3.0 < x)
+    return pow(exp(x / 2.0), 2);
+
+  auto e_x = 1.0;
+  auto last = 1.0;
+  for (int n = 1; n < 20; ++n)
+    e_x += (last *= x / static_cast<double>(n));
+  return e_x;
+}
+
+constexpr auto ln(double x) -> double
+{
+  if (x < 0)
+    throw std::invalid_argument("ln(negative)");
+
+  auto l_x = double{};
+  auto last = 2.0;
+  for (int n = 0; n < 10; ++n)
+  {
+    const auto e_last = exp(last);
+    last = (l_x += 2 * (x - e_last) / (x + e_last));
+  }
+  return l_x;
+}
+} // namespace mlp
